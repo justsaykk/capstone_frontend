b2c1a72225e01bb37040287f15db455ec40bb2e3
justsaykk <kaikein.woo@gmail.com>
2022-05-29 20:10:32 +0800

    managed to fetch data from heroku

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,14 +1,36 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import NavBar from "../components/NavBar";
 
 function ProductsPage() {
-    // Logic Here
+    const [productList, setProductList] = useState([])
+
+    // URLs
+    const BACKEND = process.env.REACT_APP_BACKEND
+
+    // API
+    const URL = BACKEND + "/api/product";
+    useEffect(() => {
+        fetch(URL)
+        .then(res => res.json())
+        .then(data => {setProductList(data)})
+    }, [])
+
+    console.log("product List = ", productList)
 
     return (
         <div>
             <NavBar />
-            <h1>Hello World!</h1>
-            <h1>You are at the Products Page</h1>
+            <div><h1>Banner here</h1></div>
+            <div><h2>Title of trek</h2></div>
+            <div>
+                <div className="image"><img></img></div>
+            </div>
+            <div className="overviewWrapper">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae tempore et ut culpa repudiandae provident exercitationem eligendi natus sint, ullam obcaecati dolore itaque. Placeat vel reprehenderit laboriosam. Pariatur, distinctio facere?</p>
+            </div>
+            <div>
+                <button>Add to Cart</button>
+            </div>
         </div>
     )
 }
